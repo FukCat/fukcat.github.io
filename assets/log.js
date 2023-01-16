f444a853245eae9a6eb18cedada47562206def97
sfundogCDN <107168358+sfundogCDN@users.noreply.github.com>
2023-01-17 07:26:17 +0800

    Update log.js

--- assets/log.js
+++ assets/log.js
@@ -1,4 +1,4 @@
-console.log('\n' + ' %c SFUNDOG' + APLAYER_VERSION + ' ' + GIT_HASH + ' %c https://sfundogcdn.github.io/ ' + '\n', 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
+console.log('\n' + ' %c SFUNDOG' + ' %c https://sfundogcdn.github.io/ ' + '\n', 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
 console.log("你瞅啥?!");
 console.info("你TM故意找茬儿是不是?");
 console.warn("你这站保熟吗?");
