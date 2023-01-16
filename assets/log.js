4f5a5166a09e331521e29687e81ba121878197be
sfundogCDN <107168358+sfundogCDN@users.noreply.github.com>
2023-01-17 07:21:54 +0800

    Add files via upload

---
+++ assets/log.js
@@ -0,0 +1,12 @@
+console.log('\n' + ' %c SFUNDOG' + APLAYER_VERSION + ' ' + GIT_HASH + ' %c https://sfundogcdn.github.io/ ' + '\n', 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
+console.log("你瞅啥?!");
+console.info("你TM故意找茬儿是不是?");
+console.warn("你这站保熟吗?");
+console.error("你干嘛哈哈哎呦!!");
+console.log("%c+",
+  `font-size: 1px;
+  padding: 122px 217px;
+  background-image: url(https://sfundogcdn.github.io/ikun/retouch_2022112517372705_edit_37499622586890.jpg);
+  background-size: contain;
+  background-repeat: no-repeat;
+  color: transparent;`);
